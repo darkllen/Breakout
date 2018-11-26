4819e6d0a39bfa184bc9342d34df440feb2b932e
Ana22102000 <45357776+Ana22102000@users.noreply.github.com>
2018-11-26 16:23:24 +0200

    Update GPaddle.java

--- src/GPaddle.java
+++ src/GPaddle.java
@@ -4,10 +4,11 @@ import acm.graphics.*;
 
 public class GPaddle {
 
-    public static void createPaddle(int x,int y,int width,int height, Main main) {
+    public static GRect createPaddle(int x,int y,int width,int height, Main main) {
          GRect paddle = new GRect(x,y,width,height);
          paddle.setFilled(true);
          main.add(paddle);
+         return(paddle);
 
 
     }
